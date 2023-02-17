c5a74f42ed417d1464fa6bf23ebd4b23a51821fd
Cyrus Chan <cyruschan1212@gmail.com>
2023-02-17 19:30:45 +0800

    Impl executor

--- lib/src/tester.dart
+++ lib/src/tester.dart
@@ -108,6 +108,160 @@ class AsserestTestAssigner {
   bool isAssigned(Type propertyType) =>
       _platformBuilders.containsKey(propertyType);
 
-  AsserestTestPlatform buildTestPlatform(AsserestProperty property) =>
+  AsserestTestPlatform _buildTestPlatform(AsserestProperty property) =>
       _platformBuilders[property.runtimeType]!(property);
+}
+
+class _AsserestParallelTestPlatformStreamSubscription
+    implements StreamSubscription<AsserestReport> {
+  final StreamSubscription<AsserestReport> _base;
+  final AsyncExecutor _executor;
+
+  _AsserestParallelTestPlatformStreamSubscription(this._base, this._executor) {
+    this.onDone(null);
+    this.onError(null);
+  }
+
+  @override
+  Future<E> asFuture<E>([E? futureValue]) {
+    return _base.asFuture<E>(futureValue);
+  }
+
+  @override
+  Future<void> cancel() async {
+    await _executor.close();
+    await _base.cancel();
+  }
+
+  @override
+  bool get isPaused => _base.isPaused;
+
+  @override
+  void onData(void Function(AsserestReport data)? handleData) {
+    _base.onData(handleData);
+  }
+
+  @override
+  void onDone(void Function()? handleDone) {
+    _base.onDone(() async {
+      await _executor.close();
+      (handleDone ?? () {})();
+    });
+  }
+
+  @override
+  void onError(Function? handleError) {
+    _base.onError((err, stackTrace) async {
+      await _executor.close();
+      if (handleError != null) {
+        handleError(err, stackTrace);
+      } else {
+        throw err;
+      }
+    });
+  }
+
+  @override
+  void pause([Future<void>? resumeSignal]) {
+    _base.pause(resumeSignal);
+  }
+
+  @override
+  void resume() {
+    _base.resume();
+  }
+}
+
+class _AsserestParallelTestTypeSet extends SetBase<AsserestTestPlatform> {
+  final HashMap<Type, AsserestTestPlatform> _typeMap = HashMap();
+
+  @override
+  bool add(AsserestTestPlatform<AsserestProperty> value) {
+    final int expLen = length + 1;
+    _typeMap.putIfAbsent(value.runtimeType, () => value);
+    return _typeMap.length == expLen;
+  }
+
+  @override
+  bool contains(Object? element) {
+    return _typeMap.containsValue(element);
+  }
+
+  @override
+  Iterator<AsserestTestPlatform<AsserestProperty>> get iterator =>
+      _typeMap.values.iterator;
+
+  @override
+  int get length => _typeMap.length;
+
+  @override
+  AsserestTestPlatform<AsserestProperty>? lookup(Object? element) {
+    return _typeMap[element];
+  }
+
+  @override
+  bool remove(Object? value) {
+    throw UnsupportedError("Removing added type assignment is forbidden.");
+  }
+
+  @override
+  Set<AsserestTestPlatform<AsserestProperty>> toSet() {
+    return _typeMap.values.toSet();
+  }
+
+  @override
+  List<AsserestTestPlatform<AsserestProperty>> toList({bool growable = false}) {
+    return _typeMap.values.toList(growable: growable);
+  }
+}
+
+typedef void AsyncExecutorLogger(String type, dynamic message,
+    [dynamic error, dynamic stackTrace]);
+
+@sealed
+class AsserestParallelTestPlatform extends IterableBase<AsserestTestPlatform> {
+  final Map<int, AsserestTestPlatform> _platforms = {};
+  final _AsserestParallelTestTypeSet _typeSet = _AsserestParallelTestTypeSet();
+
+  final int threads;
+
+  AsserestParallelTestPlatform({this.threads = 1});
+
+  @override
+  Iterator<AsserestTestPlatform<AsserestProperty>> get iterator =>
+      _platforms.values.iterator;
+
+  int get length => _platforms.length;
+
+  void apply(AsserestProperty property) {
+    AsserestTestPlatform platform =
+        AsserestTestAssigner()._buildTestPlatform(property);
+    _platforms[property.hashCode] = platform;
+  }
+
+  void appplyAll(Iterable<AsserestProperty> properites) {
+    properites.forEach(apply);
+  }
+
+  StreamSubscription<AsserestReport> runAll(
+      {String? name,
+      AsyncExecutorLogger? logger,
+      void Function(AsserestReport data)? onData,
+      void Function()? onDone,
+      Function? onError,
+      bool? cancelOnError}) {
+    final AsyncExecutor executor = AsyncExecutor(
+        name: name ?? "Asserest parallel test - ${DateTime.now()}",
+        logger: logger,
+        sequential: false,
+        parallelism: threads,
+        taskTypeRegister: _typeSet.toList);
+
+    Stream<AsserestReport> reportStream =
+        Stream.fromFutures(executor.executeAll(_platforms.values));
+    return _AsserestParallelTestPlatformStreamSubscription(
+        reportStream.listen(onData,
+            onDone: onDone, onError: onError, cancelOnError: cancelOnError),
+        executor);
+  }
 }
