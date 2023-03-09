a18de4f02e04ba0d4d162a47c8e47b72248ef874
Cyrus Chan <cyruschan1212@gmail.com>
2023-03-09 13:07:51 +0800

    Comment

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,4 +1,23 @@
-class AsserestError extends Error {
+import 'report.dart' show AsserestResult;
+
+/// A generic interface for indicating Asserest related
+/// [Error] and [Exception].
+abstract class AsserestThrowable {
+  const AsserestThrowable._();
+
+  /// An message of this throwable object.
+  String get message;
+
+  /// A [String] will be used for describe throwable information
+  /// in terminal.
+  /// 
+  /// The content should be as detail as possible.
+  @override
+  String toString();
+}
+
+/// An [Error] based throwable for any execution error in Asserest.
+class AsserestError extends Error implements AsserestThrowable {
   final String message;
 
   AsserestError([this.message = ""]);
@@ -7,7 +26,8 @@ class AsserestError extends Error {
   String toString() => "AsserestError: $message";
 }
 
-class AsserestException implements Exception {
+/// An [Exception] based throwable for unexpected operation by user.
+class AsserestException implements Exception, AsserestThrowable {
   final String message;
 
   AsserestException([this.message = ""]);
--- lib/src/report.dart
+++ lib/src/report.dart
@@ -1,21 +1,51 @@
 import 'package:meta/meta.dart';
 
+/// Enumerated value for indicating tester result.
 enum AsserestResult {
-  success, failure, error
+  /// The test result is in expected.
+  success,
+  /// The test result is difference with expected.
+  failure,
+  /// Any error thrown by non-Asserest related elements.
+  error
 }
 
+/// Report object for asserting [url]'s accessibility.
+/// 
+/// This should not be inherted or implemented to prevent
+/// modification of value.
 @sealed
 @immutable
 abstract class AsserestReport {
   const AsserestReport._();
 
+  /// Tested URL in [URI] object form.
   Uri get url;
+  
+  /// Expected the given [url] is accessible when assume no
+  /// unexpected [AsserestResult.error] returned in [actual].
   bool get expected;
+
+  /// Enumerated value of actual result after the [url] has been
+  /// asserted by tester.
   AsserestResult get actual;
+
+  /// An optional variable to counting exection duration for testing
+  /// [url].
+  /// 
+  /// If return [Null], it means either the tester does not support
+  /// counting or disabled by user.
   Duration? get executeDuration;
 
+  /// Return an unmodifiable [Map] object for storing entire
+  /// [AsserestReport] as corresponded JSON form in Dart.
+  /// 
+  /// [executeDuration] will be converted to [Duration.toString]
+  /// if applied.
   Map<String, dynamic> toMap(); 
 
+  /// Generate a [String] which contains all provided element in this
+  /// report.
   @override
   String toString();
 }
