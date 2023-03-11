f242f5b69af17b9966cb3274e23ffbf523f89509
Cyrus Chan <cyruschan1212@gmail.com>
2023-03-12 01:06:26 +0800

    Library comment

--- lib/interface.dart
+++ lib/interface.dart
@@ -1,3 +1,6 @@
+/// A barebone interface library for standarize execution on Asserest.
+library libasserest_interface;
+
 export 'src/exceptions.dart' hide AsserestThrowable;
 export 'src/property.dart';
 export 'src/report.dart';
